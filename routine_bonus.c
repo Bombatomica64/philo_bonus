3481d30aba98e41bbd708061b3a5a15205dc6587
Bombatomica64 <150276837+Bombatomica64@users.noreply.github.com>
2024-02-05 16:09:06 +0100

    Update main_process_bonus.c and routine_bonus.c (#3)

--- routine_bonus.c
+++ routine_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mruggier <mruggier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 15:27:41 by lmicheli          #+#    #+#             */
-/*   Updated: 2024/02/05 16:02:59 by mruggier         ###   ########.fr       */
+/*   Updated: 2024/02/05 15:37:23 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,7 @@ void	philo_life(t_data_bonus *data, int id)
 		philo_eat(data, id);
 		philo_sleep_think(data, id);
 	}
-//	pthread_join(data->philo[id].thread, NULL);
+	pthread_join(data->philo[id].thread, NULL);
 	pthread_join(data->philo[id].end, NULL);
 	pthread_detach(data->philo[id].end);
 	pthread_detach(data->philo[id].thread);
